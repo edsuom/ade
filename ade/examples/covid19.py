cea3e8db40435ea81ee7724883b95f35d73ba24e
server <foss@edsuom.com>
2020-03-21 12:03:43 -0700

    Example covid19.py: Attempt to introduce a square term with time offset. Bombed because of unrealistically high coefficient for the squared term coupled with unrealistically late t1. Perhaps a second fitness measure could be added minimizing the second derivative, but that is starting to seem weird.

--- ade/examples/covid19.py
+++ ade/examples/covid19.py
@@ -293,15 +293,19 @@ class Covid19Data_US(Covid19Data):
     countryCode = 'US'
     bounds = [
         # Maximum number of cases expected to be reported, ever
-        ('L',   (5e7, 8e8)),
+        ('L',   (5e7, 3.3e8)),
         # The logistic growth rate, proportional to the number of
         # cases being reported per hour at midpoint
-        ('k',   (1.2e-2, 1.6e-2)),
+        ('k',   (1.0e-2, 1.9e-2)),
         # Midpoint time (hours)
-        ('t0',  (1900, 2200)),
+        ('t0',  (1700, 2200)),
+        # Squared term
+        ('a',   (0.0, 100.0)),
+        # Offset time (hours) for squared term
+        ('t1',  (1000, 1800)),
         # Linear term (constant hourly increase in the number of
         # reported cases)
-        ('a',   (0, 0.4)),
+        ('b',   (0.0, 0.6)),
     ]
 
 
@@ -447,14 +451,19 @@ class Evaluator(Picklable):
         to increasing testing early on skewing the early case numbers
         upward) is implemented this equation:
 
-        M{x = L/(1 + exp(-k*(t-t0))) + a*t}
+        M{x = L/(1 + exp(-k*(t-t0))) + a*(t > t1 ? t-t1 : 0)^2 + b*t}
         """
-        L, k, t0, a = args
+        L, k, t0, a, t1, b = args
         inside = -k*(t-t0)
-        X = np.zeros_like(t)
+        X = np.zeros(len(t))
         K = np.flatnonzero(inside < 700)
         X[K] = L/(1 + np.exp(inside[K]))
-        return X + a*t
+        K = np.flatnonzero(t > t1)
+        try:
+            X[K] += a*(t[K] - float(t1))**2
+        except:
+            import pdb; pdb.set_trace() 
+        return X + b*t
     
     def __call__(self, values):
         """
